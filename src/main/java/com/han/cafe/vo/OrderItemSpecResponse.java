0a358c3f07146c4dfd36bc3467463af841e802c4
HanYabo <2247561875@qq.com>
2025-03-14 22:03:55 +0800

    根据id返回商品详情接口添加数据

--- src/main/java/com/han/cafe/vo/OrderItemSpecResponse.java
+++ src/main/java/com/han/cafe/vo/OrderItemSpecResponse.java
@@ -1,12 +1,14 @@
 package com.han.cafe.vo;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
+
+import lombok.Data;
 
 @Data
 public class OrderItemSpecResponse {
+    private Integer specId;
     private String specName;
     private String specValue;
+    private Integer specValueId;
     private BigDecimal extraPrice;
 }
